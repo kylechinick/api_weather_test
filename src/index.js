bd24110810bda7a4934c7e631f8939659ed79cfa
Kyle Chinick <kylechinick@icloud.com>
2022-06-26 12:30:10 -0700

    Update codebase to receive user input and complete API request

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-// import bizFunc from './js/biz-func.js';
 import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
 $(document).ready(function () {
-  $('selector').submit(function (event) {
-    event.preventDefault(); // if form
-    // more code
+  $('#weatherLocation').click(function () {
+    const city = $('#location').val();
+    $('#location').val('');
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=04258fdeac3714ff175da841716a5d60`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open('GET', url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showHumidity').text(
+        `The humidity in ${city} is ${response.main.humidity}%`
+      );
+      $('.showTemp').text(
+        `The temperature in Kelvins is ${response.main.temp} degrees.`
+      );
+    }
   });
 });
-
-console.log('Connection test');
